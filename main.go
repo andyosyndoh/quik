1549d416a0228d68691430b31951e06d56b61091
kada <adakennedy6@gmail.com>
2025-03-13 17:48:33 +0300

    [feat]: add basic command-line argument parsing
    - Implement initial argument handling for the textindex CLI.
    - Add support for the `-c` option to specify a command.
    - Exits with a usage message if no arguments, or an error if an unknown option is provided.

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+)
 
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: textindex [options]")
+		os.Exit(1)
+	}
+
+	command := ""
+	if os.Args[1] == "-c" {
+		command = os.Args[2]
+	} else {
+		fmt.Println("Unknown command option")
+		os.Exit(1)
+	}
+	args := os.Args[3:]
 }
