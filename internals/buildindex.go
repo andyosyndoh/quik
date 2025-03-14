b598909346f839eca827e7e19161d9aa9a8d5a78
masman <asmanodeny@gmail.com>
2025-03-14 14:50:42 +0300

    feat: added function to extract words and count occurrences in computeSimHash

--- internals/buildindex.go
+++ internals/buildindex.go
@@ -11,5 +11,11 @@ type resultData struct {
 
 // computeSimHash generates a SimHash for the given data using a reusable hash.Hash64.
 func computeSimHash(data []byte, h hash.Hash64) uint64 {
-	
+	text := string(data)
+	words := strings.Fields(text)
+
+	counts := make(map[string]int)
+	for _, word := range words {
+		counts[word]++
+	}
 }
