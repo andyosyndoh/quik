35e59366a7e17315ad996d29c19fb2f072ed2332
anne-okingo <annekadiso@gmail.com>
2025-03-14 16:14:32 +0300

    feat: open index file for reading

--- internals/runlookup.go
+++ internals/runlookup.go
@@ -1,6 +1,18 @@
 package internals
 
+import (
+	"fmt"
+	"os"
+)
+
 
 func RunLookup(indexFile, simHashStr string) error {
-return nil
+	// Open the index file for reading.
+	dataFile, err := os.Open(indexFile)
+	if err != nil {
+		return fmt.Errorf("error opening index file: %v", err)
+	}
+	defer dataFile.Close()
+
+	return nil
 }
