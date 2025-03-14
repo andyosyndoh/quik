00556a5db67a2faf7f662dbbe969b7c8cd8fae1b
adaken4 <adakennedy@outlook.com>
2025-03-14 17:08:25 +0300

    [feat]: decode the file contents

--- internals/index-decoder.go
+++ internals/index-decoder.go
@@ -1,16 +1,24 @@
 package internals
 
 import (
+	"encoding/gob"
 	"fmt"
 	"os"
 )
 
 func IndexFileDecoder(file string) error {
+	// Load index file into memory
 	dataFile, err := os.Open(file)
 	if err != nil {
 		return fmt.Errorf("error opening index file: %w", err)
 	}
 	defer dataFile.Close()
+
+	// Decode the file content into IndexData struct
+	var indexData IndexData
+	if err := gob.NewDecoder(dataFile).Decode(&indexData); err != nil {
+		return fmt.Errorf("error decoding index data: %v\n", err)
+	}
 
 	return nil
 }
