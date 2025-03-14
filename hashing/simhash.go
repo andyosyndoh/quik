fea1c06951567e952bbda9371f6dc00f21896430
Andy Osyndoh <andyovvo8@gmail.com>
2025-03-14 12:36:29 +0300

    chore: populate the words to a map to count the occurences

--- hashing/simhash.go
+++ hashing/simhash.go
@@ -10,5 +10,11 @@ func computeSimHash(data []byte, h hash.Hash64) uint64 {
 	var simhash uint64
 	text := string(data)
 	words := strings.Fields(text)
+
+	counts := make(map[string]int)
+	for _, word := range words {
+		counts[word]++
+	}
+	
 	return simhash
 }
