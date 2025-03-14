5a8f24b63b6ef8e57ce34322e11d2c9f7f1c0bca
masman <asmanodeny@gmail.com>
2025-03-14 14:49:04 +0300

    feat: initialized a computeSimHash function for SimHash computation

--- internals/buildindex.go
+++ internals/buildindex.go
@@ -8,3 +8,8 @@ type resultData struct {
 	simhash uint64
 	offset  int64
 }
+
+// computeSimHash generates a SimHash for the given data using a reusable hash.Hash64.
+func computeSimHash(data []byte, h hash.Hash64) uint64 {
+	
+}
