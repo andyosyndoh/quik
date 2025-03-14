cdf1f0525d2970b67e0b3561824d612de301c5ee
anne-okingo <annekadiso@gmail.com>
2025-03-14 16:16:27 +0300

    feat: decode the index data

--- internals/runlookup.go
+++ internals/runlookup.go
@@ -1,10 +1,10 @@
 package internals
 
 import (
+	"encoding/gob"
 	"fmt"
 	"os"
 )
-
 
 func RunLookup(indexFile, simHashStr string) error {
 	// Open the index file for reading.
@@ -13,6 +13,13 @@ func RunLookup(indexFile, simHashStr string) error {
 		return fmt.Errorf("error opening index file: %v", err)
 	}
 	defer dataFile.Close()
+
+	// Decode the index data from the file.
+	var indexData IndexData
+	decoder := gob.NewDecoder(dataFile)
+	if err := decoder.Decode(&indexData); err != nil {
+		return fmt.Errorf("error decoding index data: %v", err)
+	}
 
 	return nil
 }
