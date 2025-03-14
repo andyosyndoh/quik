e4063874796111a29ba3df941a96a77bf7060885
masman <asmanodeny@gmail.com>
2025-03-14 15:15:13 +0300

    feat: Define BuildIndex method for file indexing

--- internals/buildindex.go
+++ internals/buildindex.go
@@ -1,10 +1,5 @@
 package internals
 
-import (
-	"hash"
-	"strings"
-)
-
 type chunkData struct {
 	data   []byte
 	offset int64
@@ -14,34 +9,7 @@ type resultData struct {
 	offset  int64
 }
 
-// computeSimHash generates a SimHash for the given data using a reusable hash.Hash64.
-func computeSimHash(data []byte, h hash.Hash64) uint64 {
-	text := string(data)
-	words := strings.Fields(text)
+// BuildIndex processes the file and builds the in-memory index.
+func (fi *FileIndex) BuildIndex(filename string) error {
 
-	counts := make(map[string]int)
-	for _, word := range words {
-		counts[word]++
-	}
-	var sums [64]int
-	for word, cnt := range counts {
-		h.Reset()
-		h.Write([]byte(word))
-		hash := h.Sum64()
-
-		for i := 0; i < 64; i++ {
-			if (hash & (1 << i)) != 0 {
-				sums[i] += cnt
-			} else {
-				sums[i] -= cnt
-			}
-		}
-	}
-	var simhash uint64
-	for i := 0; i < 64; i++ {
-		if sums[i] >= 0 {
-			simhash |= 1 << i
-		}
-	}
-	return simhash
 }
