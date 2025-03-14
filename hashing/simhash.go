c676bfc29bc0b16575f60b47ec288a3f71f2b529
Andy Osyndoh <andyovvo8@gmail.com>
2025-03-14 12:34:00 +0300

    chore: split the data into words

--- hashing/simhash.go
+++ hashing/simhash.go
@@ -1,3 +1,14 @@
 package hashing
 
+import (
+	"hash"
+	"strings"
+)
+
 // computeSimHash generates a SimHash for the given data using a reusable hash.Hash64.
+func computeSimHash(data []byte, h hash.Hash64) uint64 {
+	var simhash uint64
+	text := string(data)
+	words := strings.Fields(text)
+	return simhash
+}
