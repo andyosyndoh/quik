6317dcd9bdda50aa8dd22ae4ace6ab668cc0b7f5
andyosyndoh <andyovvo8@gmail.com>
2025-03-15 00:27:21 +0300

    chore: read idx file

--- internals/runfuzzy.go
+++ internals/runfuzzy.go
@@ -1,6 +1,16 @@
 package internals
 
+import (
+	"fmt"
+	"os"
+)
+
 // RunFuzzy performs a fuzzy search for SimHashes with a Hamming distance of 1 or 2.
 func RunFuzzy(indexFile, simHashStr string) error {
+	dataFile, err := os.Open(indexFile)
+	if err != nil {
+		return fmt.Errorf("error opening index file: %v", err)
+	}
+	defer dataFile.Close()
 	return nil
 }
