9a9713922912d18d4c65a7605f6e1dacbc51148b
Andy Osyndoh <andyovvo8@gmail.com>
2025-03-14 14:04:05 +0300

    chore: initialized variables to be used to calsulate simhash for a dataset

--- internals/simhash.go
+++ internals/simhash.go
@@ -1,4 +1,15 @@
 package internals
 
+import (
+	"hash"
+	"strings"
+)
 
 // computeSimHash generates a SimHash for the given data using a reusable hash.Hash64.
+func computeSimHash(data []byte, h hash.Hash64) uint64 {
+	var simhash uint64
+	var sums [64]int
+	text := string(data)
+	words := strings.Fields(text)
+	return simhash
+}
