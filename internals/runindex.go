a67a8efaa0177159a589ab09b2c9108e0b7ac1e8
hanapiko <apikojuma94@gmail.com>
2025-03-14 15:45:47 +0300

    Initialize FileIndex and build index

---
+++ internals/runindex.go
@@ -0,0 +1,14 @@
+package internals
+
+import (
+	"fmt"
+	"runtime"
+)
+
+func runIndex(inputFile string, chunkSize int, outputFile string) error {
+	fi := NewFileIndex(chunkSize, runtime.NumCPU())
+	if err := fi.BuildIndex(inputFile); err != nil {
+		return fmt.Errorf("error building index: %v", err)
+	}
+	return nil
+}
