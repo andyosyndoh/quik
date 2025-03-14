38473ed0f8e66efa092b27d60cff5ae7f3b82827
andyosyndoh <andyovvo8@gmail.com>
2025-03-14 22:00:38 +0300

    chore: create a file to store simhashes and offsets

--- internals/index-decoder.go
+++ internals/index-decoder.go
@@ -2,19 +2,19 @@ package internals
 
 import (
 	"fmt"
+	"os"
 )
 
 // IndexFileDecoder decodes the index file and prints the metadata and index data.
 func IndexFileDecoder(indexData IndexData) error {
 	fmt.Printf("Original file: %s\n", indexData.FileName)
 	fmt.Printf("Chunk size: %d bytes\n", indexData.ChunkSize)
-	fmt.Println("SimHash values and byte offsets:")
+	fmt.Println("SimHash values and byte offsets writen to simhash.txt")
 
-	for simhash, offsets := range indexData.Index {
-		fmt.Printf("SimHash: %x\n", simhash)
-		for _, offset := range offsets {
-			fmt.Printf("  Byte offset: %d\n", offset)
-		}
+	hashfile, err := os.Create("simhash.txt")
+	if err != nil {
+		return fmt.Errorf("error creating hash file: %w", err)
 	}
+	defer hashfile.Close()
 	return nil
 }
