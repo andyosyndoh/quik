ac47847c87c6faebd1c270bace94a0f245905bd5
adaken4 <adakennedy@outlook.com>
2025-03-14 17:12:17 +0300

    [feat]: print the file name, chunk size and decoded simhash values and byte offsets in the terminal

--- internals/index-decoder.go
+++ internals/index-decoder.go
@@ -20,5 +20,16 @@ func IndexFileDecoder(file string) error {
 		return fmt.Errorf("error decoding index data: %v\n", err)
 	}
 
+	fmt.Printf("Original file: %s\n", indexData.FileName)
+	fmt.Printf("Chunk size: %d bytes\n", indexData.ChunkSize)
+	fmt.Println("SimHash values and byte offsets:")
+
+	
+	for simhash, offsets := range indexData.Index {
+		fmt.Printf("SimHash: %x\n", simhash)
+		for _, offset := range offsets {
+			fmt.Printf("  Byte offset: %d\n", offset)
+		}
+	}
 	return nil
 }
