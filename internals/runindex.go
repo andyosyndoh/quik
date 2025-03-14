e409fd22c01987568ac7619d61752e09ae84480f
hanapiko <apikojuma94@gmail.com>
2025-03-14 15:48:21 +0300

    prepare index data structure

--- internals/runindex.go
+++ internals/runindex.go
@@ -10,5 +10,11 @@ func runIndex(inputFile string, chunkSize int, outputFile string) error {
 	if err := fi.BuildIndex(inputFile); err != nil {
 		return fmt.Errorf("error building index: %v", err)
 	}
+
+	indexData := IndexData{
+		FileName:  inputFile,
+		ChunkSize: chunkSize,
+		Index:     fi.index.m,
+	}
 	return nil
 }
